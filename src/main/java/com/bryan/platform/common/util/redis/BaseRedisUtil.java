775d5b6f51a5d0ce7d5da37aae27fe6ee95ef0e3
Bryan Long <bryantaolong@outlook.com>
2025-06-21 17:21:12 +0800

    Add Redis Utils

---
+++ src/main/java/com/bryan/platform/common/util/redis/BaseRedisUtil.java
@@ -0,0 +1,20 @@
+package com.bryan.platform.common.util.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * ClassName: BaseRedisUtil
+ * Package: com.bryan.platform.common.util.redis
+ * Description: Redis 工具类的抽象基类，提供 RedisTemplate 的通用注入。
+ * 将 RedisTemplate 的泛型明确为 <String, Object>，与 RedisConfig 配置保持一致。
+ * Author: Bryan Long
+ * Create: 2025/6/20 - 17:27
+ * Version: v1.0
+ */
+@RequiredArgsConstructor
+public abstract class BaseRedisUtil {
+    // 明确 RedisTemplate 的泛型为 <String, Object>
+    // 对应 RedisConfig 中 KeySerializer 为 String，ValueSerializer 为 JSON (Object)
+    protected final RedisTemplate<String, Object> redisTemplate;
+}
