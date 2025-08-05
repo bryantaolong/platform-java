0606cb2aff53b5b92985f08d4a0a27d7c0a461ac
Bryan Long <bryantaolong@outlook.com>
2025-08-05 21:00:23 +0800

    Fix MyBatis Plus MetaObjectHandler's implementation CreateUpdateHandler

--- src/main/java/com/bryan/platform/config/AppConfig.java
+++ src/main/java/com/bryan/platform/config/AppConfig.java
@@ -1,17 +1,17 @@
 package com.bryan.platform.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * * AppConfig Application configuration class
- * *
- * * @author Bryan Long
- * * @version 1.0
- * * @since 2025/6/25-11:20
+ * AppConfig Application configuration class
+ *
+ * @author Bryan Long
+ * @version 1.0
+ * @since 2025/6/25-11:20
  */
-
 @Configuration
 public class AppConfig {
 
