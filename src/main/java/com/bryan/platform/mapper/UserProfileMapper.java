6fe5bebcc3cbe90a61979d0431e40e26cf9e3470
Bryan Long <bryantaolong@outlook.com>
2025-08-21 15:32:52 +0800

    Implement UserProfileMapper

--- src/main/java/com/bryan/platform/mapper/UserProfileMapper.java
+++ src/main/java/com/bryan/platform/mapper/UserProfileMapper.java
@@ -1,5 +1,6 @@
 package com.bryan.platform.mapper;
 
+import com.bryan.platform.domain.entity.user.UserProfile;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
@@ -9,4 +10,12 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface UserProfileMapper {
+
+    int insert(UserProfile record);
+
+    UserProfile selectByUserId(Long userId);
+
+    UserProfile selectByRealName(String realName);
+
+    int update(UserProfile record);
 }
